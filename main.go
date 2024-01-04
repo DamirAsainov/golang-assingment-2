029b2bec83178fa593b8278586788b1447211ff4
nek07 <abylaiooaooa@mail.ru>
2024-01-04 23:09:39 +0600

    form can send data to go server

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/submit", submitHandler)
+
+	port := 8080
+	fmt.Printf("Server is running on http://localhost:%d\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "form.html")
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+		name := r.FormValue("name")
+		password := r.FormValue("password")
+		fmt.Fprintf(w, "name = %s\n", name)
+		fmt.Fprintf(w, "password = %s", password)
+
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
